0fe36f81762f0ba74559ba5be2c51b500697069e
medchij <udemchig@gmail.com>
2023-03-03 13:33:23 +0800

    Oorchlolt oruullaa

--- slide-stories.js
+++ slide-stories.js
@@ -63,23 +63,23 @@ class SlideStories {
 }
 new SlideStories('slide');
 
-var track = document.getElementById('track');
-var controlBtn = document.getElementById('play-pause');
-function playPause() {
-    if (track.paused) {
-        track.play();
-        //controlBtn.textContent = "Pause";
-        controlBtn.className = "pause";
-    } else { 
-        track.pause();
-         //controlBtn.textContent = "Play";
-        controlBtn.className = "play";
-    }
-}
-controlBtn.addEventListener("click", playPause);
-track.addEventListener("ended", function() {
-  controlBtn.className = "play";
-});
+// var track = document.getElementById('track');
+// var controlBtn = document.getElementById('play-pause');
+// function playPause() {
+//     if (track.paused) {
+//         track.play();
+//         //controlBtn.textContent = "Pause";
+//         controlBtn.className = "pause";
+//     } else { 
+//         track.pause();
+//          //controlBtn.textContent = "Play";
+//         controlBtn.className = "play";
+//     }
+// }
+// controlBtn.addEventListener("click", playPause);
+// track.addEventListener("ended", function() {
+//   controlBtn.className = "play";
+// });
 // var play = document.querySelector('.play');
 // var pause = document.querySelector('.pause');
 // var animation1 = document.querySelector('.slide-thumb > span.active')
